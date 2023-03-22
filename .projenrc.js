bc11a442bf23fa4b955c3efd9f07d00069e31d6c
Kiarash Kiani <kiarash.kiani@icloud.com>
2023-03-22 16:57:28 +0100

    fix: change the author name and email to datachef

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { awscdk } = require("projen");
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: "Kiarash Kiani",
-  authorAddress: "Kiarash@DataChef.co",
+  author: "DataChef",
+  authorAddress: "support@datachef.co",
   cdkVersion: "2.1.0",
   defaultReleaseBranch: "main",
   name: "cost-monitoring-construct",
